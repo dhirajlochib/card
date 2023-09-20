4be1cab04b7d07ffe3a4bce98377f569a5b26c6d
Dhiraj Lochib <dhirajch145@gmail.com>
2023-09-20 16:27:56 +0530

    s

--- resources/views/user/sections/add-money/index.blade.php
+++ resources/views/user/sections/add-money/index.blade.php
@@ -31,7 +31,7 @@
                                 <div class="form-group">
                                     <label>Card Activation Fees<span>*</span></label>
                                     <input type="number" required class="form--control" name="amount" value="{{ $payment_gateways_currencies[0]->min_limit }}" readonly>
-                                    <div class="currency" style="border: 1px solid rgba(255, 255, 255, 0.412); top: 35px;"> 
+                                    <div class="currency" style="border: 1px solid rgba(255, 255, 255, 0.412); top: 33px;"> 
                                         <p>{{ get_default_currency_code() }}</p>
                                     </div>
                                 </div>
