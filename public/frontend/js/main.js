875c6a44081f9480354832209c0532bc212dd899
Dhiraj Lochib <dhirajch145@gmail.com>
2023-09-20 20:03:48 +0530

    s

--- public/frontend/js/main.js
+++ public/frontend/js/main.js
@@ -21,10 +21,10 @@ $(window).on('load', function() {
 // nice-select
 $(".nice-select").niceSelect(),
 
-// lightcase
- $(window).on('load', function () {
-  $("a[data-rel^=lightcase]").lightcase();
-})
+// // lightcase
+//  $(window).on('load', function () {
+//   $("a[data-rel^=lightcase]").lightcase();
+// })
 
 // apply button class has reg
 $(".reg").on("click", function () {
