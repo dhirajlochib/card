34d5c2efb596e8b40541a25e491fd10f4c8d8597
naaibooking <naaibooking@gmail.com>
2023-09-24 12:27:57 +0530

    a

--- resources/views/frontend/sections/about.blade.php
+++ resources/views/frontend/sections/about.blade.php
@@ -33,46 +33,48 @@
 
 </style>
 <section class="ptb-80">
+    <!-- title in center -->
+    <h4 class="text-center" style="color: black;">{{ __("Offers & Discounts") }}</h4>
 <div class="slider">
 	<div class="slide-track">
 		<div class="slide">
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/2.png" height="100" width="250" alt="" />
+			<img src="{{asset('public/fileHolder/2.png')}}" height="100" width="250" alt="" />
 		</div>
 		<div class="slide">
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/3.png" height="100" width="250" alt="" />
+			<img src="{{asset('public/fileHolder/3.png')}}" height="100" width="250" alt="" />
 		</div>
 		<div class="slide">
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/4.png" height="100" width="250" alt="" />
+			<img src="{{asset('public/fileHolder/4.png')}}" height="100" width="250" alt="" />
 		</div>
 		<div class="slide">
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/5.png" height="100" width="250" alt="" />
+			<img src="{{asset('public/fileHolder/5.png')}}" height="100" width="250" alt="" />
 		</div>
 		<div class="slide">
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/6.png" height="100" width="250" alt="" />
+			<img src="{{asset('public/fileHolder/6.png')}}" height="100" width="250" alt="" />
 		</div>
 		<div class="slide">
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/7.png" height="100" width="250" alt="" />
+			<img src="{{asset('public/fileHolder/7.png')}}" height="100" width="250" alt="" />
 		</div>
 		<div class="slide">
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/1.png" height="100" width="250" alt="" />
+			<img src="{{asset('public/fileHolder/1.png')}}" height="100" width="250" alt="" />
 		</div>
 		<div class="slide">
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/2.png" height="100" width="250" alt="" />
+			<img src="{{asset('public/fileHolder/2.png')}}" height="100" width="250" alt="" />
 		</div>
 		<div class="slide">
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/3.png" height="100" width="250" alt="" />
+			<img src="{{asset('public/fileHolder/3.png')}}" height="100" width="250" alt="" />
 		</div>
 		<div class="slide">
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/4.png" height="100" width="250" alt="" />
+			<img src="{{asset('public/fileHolder/4.png')}}" height="100" width="250" alt="" />
 		</div>
 		<div class="slide">
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/5.png" height="100" width="250" alt="" />
+			<img src="{{asset('public/fileHolder/5.png')}}" height="100" width="250" alt="" />
 		</div>
 		<div class="slide">
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/6.png" height="100" width="250" alt="" />
+			<img src="{{asset('public/fileHolder/6.png')}}" height="100" width="250" alt="" />
 		</div>
 		<div class="slide">
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/7.png" height="100" width="250" alt="" />
+			<img src="{{asset('public/fileHolder/7.png')}}" height="100" width="250" alt="" />
 		</div>
 	</div>
 </div>
