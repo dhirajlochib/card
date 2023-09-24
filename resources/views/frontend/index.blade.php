ab29c7d6ca0e83c8dd55a56ae42c26cfa9a1fa0c
naaibooking <naaibooking@gmail.com>
2023-09-24 17:16:09 +0530

    d

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -26,7 +26,7 @@ $banner = App\Models\Admin\SiteSections::getData( $banner_slug)->first();
                     <h1 class="title">{{ @$heading[0] }} <br> {{ @$heading[4] }} {{ @$heading[5] }} <br> <span class="text--base" style="background-color: transparent; -webkit-text-fill-color: transparent; background-clip: text; -webkit-background-clip: text; background-image: linear-gradient(160deg, #DCFFEA 0%, #1B756B 53%); font-size: 75px; line-height: 0.8;"> {{ @$heading[6] }}</span> {{ @$heading[7] }} {{ @$heading[8] }}</h1>
                     <p>{{ __(@$banner->value->language->$lang->sub_heading) }}</p>
                     <div class="banner-btn">
-                        <button class="btn--base reg-dashboard-btn"><i aria-hidden="true" class="far fa-credit-card" style="color: white;"></i> &nbsp;{{ __(@$banner->value->language->$lang->button_name) }}</button>
+                        <button id="reg-dashboard-btn" class="btn--base reg-dashboard-btn"><i aria-hidden="true" class="far fa-credit-card" style="color: white;"></i> &nbsp;{{ __(@$banner->value->language->$lang->button_name) }}</button>
                         <a class="text--base" href="{{ route('about') }}">
                             <span class="base--text" style="color: #1b756b;"> &nbsp; Learn More <i class="fas fa-arrow-right"></i></span>
                         </a>
@@ -50,7 +50,7 @@ $banner = App\Models\Admin\SiteSections::getData( $banner_slug)->first();
     if (ref == "facebook") {
       //$(".reg-dashboard-btn").trigger("click");
     document.getElementById("reg-dashboard-btn").click();
-    
+
     }
     </script>
 
