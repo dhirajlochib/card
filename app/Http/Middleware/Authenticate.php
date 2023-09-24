680c1e1ad1c839b01ee2f11785127c41918fe3b1
naaibooking <naaibooking@gmail.com>
2023-09-24 14:55:14 +0530

    d

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Illuminate\Support\Facades\DB;
 
 class Authenticate extends Middleware
 {
@@ -22,6 +23,5 @@ class Authenticate extends Middleware
             }
             return route('index');
         }
-
     }
 }
