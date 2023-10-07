df6ebaef2a6f5a3a2e06cf96bd3bcbaa8398275a
Dhiraj Lochib <dhirajch145@gmail.com>
2023-10-07 10:13:56 +0530

    s

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -140,7 +140,7 @@
     })
 </script>
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.4.0/jspdf.umd.min.js"></script>
+
 @stack('script')
 
 </body>
--- resources/views/admin/sections/user-care/index.blade.php
+++ resources/views/admin/sections/user-care/index.blade.php
@@ -40,40 +40,43 @@
     </div>
 
 @endsection
-
 @push('script')
     <script>
-        itemSearch($("input[name=user_search]"),$(".user-search-table"),"{{ setRoute('admin.users.search') }}");
+        itemSearch($("input[name=user_search]"), $(".user-search-table"), "{{ setRoute('admin.users.search') }}");
     </script>
-    
-<!-- Your custom script -->
-<script>
-    // Function to export the table data to a PDF
-    function exportToPDF() {
-        // Create a new jsPDF instance
-        const pdf = new jsPDF();
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.4.0/jspdf.umd.min.js"></script>
+    <script>
+        // Function to export the table data to a PDF
+        function exportToPDF() {
+            // Check if jsPDF is defined
+            if (typeof jsPDF !== 'undefined') {
+                // Create a new jsPDF instance
+                const pdf = new jsPDF();
 
-        // Add a title to the PDF
-        pdf.text("User Data", 10, 10);
+                // Add a title to the PDF
+                pdf.text("User Data", 10, 10);
 
-        // Get the table element by its ID
-        const table = document.getElementById('myTable');
+                // Get the table element by its ID
+                const table = document.getElementById('myTable');
 
-        // Convert the table to a data URL
-        const tableDataURL = table.toDataURL();
+                // Convert the table to a data URL
+                const tableDataURL = table.toDataURL();
 
-        // Add the table as an image to the PDF
-        pdf.addImage(tableDataURL, 'PNG', 10, 20, 180, 0);
+                // Add the table as an image to the PDF
+                pdf.addImage(tableDataURL, 'PNG', 10, 20, 180, 0);
 
-        // Save or download the PDF
-        pdf.save('user_data.pdf');
-    }
+                // Save or download the PDF
+                pdf.save('user_data.pdf');
+            } else {
+                console.error("jsPDF is not defined. Make sure the library is loaded.");
+            }
+        }
 
-    // Attach the exportToPDF function to the button click event
-    $(document).ready(function() {
-        $('#exportButton').click(function() {
-            exportToPDF();
+        // Attach the exportToPDF function to the button click event
+        $(document).ready(function() {
+            $('#exportButton').click(function() {
+                exportToPDF();
+            });
         });
-    });
-</script>
+    </script>
 @endpush
