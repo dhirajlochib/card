a46d3c4ea3b1083ff583125b29510da8ac34214a
Dhiraj Lochib <dhirajch145@gmail.com>
2023-10-07 09:32:29 +0530

    s

--- resources/views/admin/sections/user-care/index.blade.php
+++ resources/views/admin/sections/user-care/index.blade.php
@@ -24,6 +24,12 @@
         <div class="table-wrapper">
             <div class="table-header">
                 <h5 class="title">{{ __("All Users") }}</h5>
+                <div class="actions">
+                    <button class="btn btn-sm btn-primary" onclick="exportUserData()">
+                        <i class="fas fa-file-export"></i>
+                        {{ __("Export Data") }}
+                    </button>
+                </div>
                 <div class="table-btn-area">
                     @include('admin.components.search-input',[
                         'name'  => 'user_search',
@@ -41,5 +47,28 @@
 @push('script')
     <script>
         itemSearch($("input[name=user_search]"),$(".user-search-table"),"{{ setRoute('admin.users.search') }}");
+        
+        function exportUserData() {
+
+            // export the users table to pdf
+            var doc = new jsPDF('p', 'pt');
+            var elem = document.getElementById("user-table");
+            var res = doc.autoTableHtmlToJson(elem);
+            doc.autoTable(res.columns, res.data, {
+                startY: 60,
+                theme: 'grid',
+                styles: {
+                    overflow: 'linebreak',
+                    fontSize: 8,
+                    cellPadding: 5,
+                    overflowColumns: 'linebreak',
+                    halign: 'center',
+                    valign: 'middle',
+                }
+            });
+            doc.save("users.pdf");
+            
+        }
+
     </script>
 @endpush
