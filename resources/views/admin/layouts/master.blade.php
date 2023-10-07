1f84fdbee9c2ae57eca4bd06a3299975972d1250
Dhiraj Lochib <dhirajch145@gmail.com>
2023-10-07 09:51:02 +0530

    s

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -30,9 +30,9 @@
 
     <!-- Fileholder CSS CDN -->
     <link rel="stylesheet" href="https://cdn.appdevs.net/fileholder/v1.0/css/fileholder-style.css" type="text/css">
+    
     <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
-
     <!-- main style css link -->
     <link rel="stylesheet" href="{{ asset('public/backend/css/style.css') }}">
 
